f1d1f41d1a13621eb3110156b97514c89e3c2564
gocove <letlite@gmail.com>
2025-04-13 12:00:13 -0700

    add leave balance

--- src/Employee.java
+++ src/Employee.java
@@ -3,11 +3,19 @@ public class Employee {
     private String name;
     private String department;
     private String email;
+    private int leaveBalance = 20;
 
     public Employee(int employeeId, String name, String department, String email) {
         this.employeeId = employeeId;
         this.name = name;
         this.department = department;
         this.email = email;
+    }
+    public void setLeaveBalance(int leaveBalance) {
+        if (leaveBalance >= 0) {
+            this.leaveBalance = leaveBalance;
+        } else {
+            System.out.println("Leave balance cannot be negative.");
+        }
     }
 }
